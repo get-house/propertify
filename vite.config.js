d543d6c32342cd304ecd2277ba0761cb1d50dc1e
Hussain Aminu <hussain_aminu@yahoo.com>
2023-07-31 00:06:54 +0300

    (): WIP

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// import fs from 'fs';
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+// import { homedir } from 'os';
+// import { resolve } from 'path';
 
+// let host = 'propertify.test';
 export default defineConfig({
     plugins: [
         laravel({
@@ -18,4 +22,31 @@ export default defineConfig({
             },
         }),
     ],
+
+    // server: detectServerConfig(host),
 });
+
+// function detectServerConfig(host) {
+//     let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`);
+//     let certificatePath = resolve(
+//         homedir(),
+//         `.config/valet/Certificates/${host}.crt`
+//     );
+//
+//     if (!fs.existsSync(keyPath)) {
+//         return {};
+//     }
+//
+//     if (!fs.existsSync(certificatePath)) {
+//         return {};
+//     }
+//
+//     return {
+//         hmr: { host },
+//         host,
+//         https: {
+//             key: fs.readFileSync(keyPath),
+//             cert: fs.readFileSync(certificatePath),
+//         },
+//     };
+// }
