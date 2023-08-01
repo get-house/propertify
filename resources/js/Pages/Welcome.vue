f10e5a4aeb4c172f85acf1d8cfb4bb9b1bfeff66
Hussain Aminu <hussain_aminu@yahoo.com>
2023-08-01 16:00:45 +0300

    (): WIP

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,20 +1,25 @@
-<script setup>
-import { Head, Link } from '@inertiajs/vue3';
+<script lang="ts" setup>
+import {Head, Link, usePage} from '@inertiajs/vue3';
+import type {PropType} from "vue";
+import {computed} from "vue";
 
 defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
+    canLogin: Boolean as PropType<boolean>,
+    canRegister: Boolean as PropType<boolean>,
+    laravelVersion: String as PropType<string>,
+    phpVersion: String as PropType<string>,
 });
+
+const authUser = computed(() => usePage().props?.auth?.user);
+
 </script>
 
 <template>
-    <Head title="Welcome" />
+    <Head title="Welcome" ></Head>
 
     <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-            <Link v-if="$page.props.auth.user" :href="route('dashboard')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
+            <Link v-if="authUser" :href="route('dashboard')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</Link>
 
             <template v-else>
                 <Link :href="route('login')" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</Link>
